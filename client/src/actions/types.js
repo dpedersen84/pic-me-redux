a4810ec545870cfc81a4635ac6070f46193fbedd
Dan <dpedersen84@gmail.com>
2019-01-29 19:11:03 -0600

    question_loading and get_question action types

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,3 +1,6 @@
 export const GET_ERRORS = "GET_ERRORS";
 export const CLEAR_ERRORS = "CLEAR_ERRORS";
 export const SET_CURRENT_USER = "SET_CURRENT_USER";
+
+export const QUESTION_LOADING = "QUESTION_LOADING";
+export const GET_QUESTION = "GET_QUESTION";
