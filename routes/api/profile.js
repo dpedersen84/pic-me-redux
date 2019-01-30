95201fd8f30ea63d9a8ffd1b88307afa9549ed0d
Dan <dpedersen84@gmail.com>
2019-01-30 13:34:07 -0600

    TODO: profile routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const passport = require("passport");
 
-// TODO: Validation
+// Input Validation
+const validateProfileInput = require("../../validation/profile");
 
 const Profile = require("../../models/Profile");
 const User = require("../../models/User");
@@ -12,5 +13,25 @@ const User = require("../../models/User");
 // @description   Tests profile route
 // @access        Public
 router.get("/test", (req, res) => res.json({ msg: "Profile Route Works" }));
+
+// @route         POST api/profile
+// @description   Create or edit user's profile
+// @access        Private
+
+// @route         GET api/profile/
+// @description   Get current users profile
+// @access        Private
+
+// @route         GET api/profile/handle/:handle
+// @description   Get profile by handle
+// @access        Public
+
+// @route         GET api/profile/user/:user_id
+// @description   Get profile by user id
+// @access        Public
+
+// @route         DELETE api/profile/
+// @description   Delete user and profile
+// @access        Private
 
 module.exports = router;
