aca6a6022101436e9ef06e397c01f2a28f1ce693
Dan <dpedersen84@gmail.com>
2019-01-30 13:45:25 -0600

    types updated with pic action types

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -4,3 +4,10 @@ export const SET_CURRENT_USER = "SET_CURRENT_USER";
 
 export const QUESTION_LOADING = "QUESTION_LOADING";
 export const GET_QUESTION = "GET_QUESTION";
+
+export const PIC_LOADING = "PIC_LOADING";
+export const GET_PICS = "GET_PICS";
+export const GET_PIC = "GET_PIC";
+export const GET_USER_PICS = "GET_USER_PICS";
+export const ADD_PIC = "ADD_PIC";
+export const DELETE_PIC = "DELETE_PIC";
