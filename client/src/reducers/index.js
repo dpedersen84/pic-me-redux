6ba5a883f4c4afb21ebb1b3ea64132d0a6800f0b
Dan <dpedersen84@gmail.com>
2019-01-31 10:11:48 -0600

    questionReducer and picReducer combined

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from "redux";
 import authReducer from "./authReducer";
 import errorReducer from "./errorReducer";
+import questionReducer from "./questionReducer";
+import picReducer from "./picReducer";
 
 export default combineReducers({
   auth: authReducer,
-  errors: errorReducer
+  errors: errorReducer,
+  questions: questionReducer,
+  pics: picReducer
 });
