93331b0f8248c6d242696f0163e884168d6f1971
Dan <dpedersen84@gmail.com>
2019-02-02 00:37:50 -0600

    added profileReducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,10 +3,12 @@ import authReducer from "./authReducer";
 import errorReducer from "./errorReducer";
 import questionReducer from "./questionReducer";
 import picReducer from "./picReducer";
+import profileReducer from "./profileReducer";
 
 export default combineReducers({
   auth: authReducer,
   errors: errorReducer,
   questions: questionReducer,
-  pics: picReducer
+  pics: picReducer,
+  profile: profileReducer
 });
