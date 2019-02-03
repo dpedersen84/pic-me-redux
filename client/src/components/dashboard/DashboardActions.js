98fda7ef5787f896af32972e4fe392d369752317
Dan <dpedersen84@gmail.com>
2019-02-03 10:23:57 -0600

    update - add pics button

--- client/src/components/dashboard/DashboardActions.js
+++ client/src/components/dashboard/DashboardActions.js
@@ -4,13 +4,17 @@ import { Link } from "react-router-dom";
 const DashboardActions = () => {
   return (
     <div className="btn-group mb4" role="group">
-      <Link to="edit-profile" className="btn btn-warning">
+      <Link to="edit-profile" className="btn btn-light">
         <i className="fas fa-user-circle text-info mr-1" />
         Edit Profile
       </Link>
-      <Link to="edit-pics" className="btn btn-warning">
+      <Link to="edit-pics" className="btn btn-light">
         <i className="fas fa-images text-info mr-1" />
         Edit Pics
+      </Link>
+      <Link to="add-pics" className="btn btn-light">
+        <i className="fas fa-images text-info mr-1" />
+        Add Pics
       </Link>
     </div>
   );
