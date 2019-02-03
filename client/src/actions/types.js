42c82b5f0b69ba03a0f30d72a508a012edb7c196
Dan <dpedersen84@gmail.com>
2019-02-03 10:26:08 -0600

    profile types

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -2,6 +2,10 @@ export const GET_ERRORS = "GET_ERRORS";
 export const CLEAR_ERRORS = "CLEAR_ERRORS";
 export const SET_CURRENT_USER = "SET_CURRENT_USER";
 
+export const GET_PROFILE = "GET_PROFILE";
+export const PROFILE_LOADING = "PROFILE_LOADING";
+export const CLEAR_CURRENT_PROFILE = "CLEAR_CURRENT_PROFILE";
+
 export const QUESTION_LOADING = "QUESTION_LOADING";
 export const GET_QUESTION = "GET_QUESTION";
 
@@ -11,3 +15,4 @@ export const GET_PIC = "GET_PIC";
 export const GET_USER_PICS = "GET_USER_PICS";
 export const ADD_PIC = "ADD_PIC";
 export const DELETE_PIC = "DELETE_PIC";
+export const SEARCH_PIC = "SEARCH_PIC";
