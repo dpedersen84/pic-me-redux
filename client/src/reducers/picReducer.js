f27524068706e6b4d87480f1f678f57244f920df
Dan <dpedersen84@gmail.com>
2019-02-11 19:54:47 -0600

    case for user pics, add pic, delete pic

--- client/src/reducers/picReducer.js
+++ client/src/reducers/picReducer.js
@@ -26,6 +26,28 @@ export default function(state = initialState, action) {
         pics: action.payload,
         loading: false
       };
+    case GET_PIC:
+      return {
+        ...state,
+        pic: action.payload,
+        loading: false
+      };
+    case GET_USER_PICS:
+      return {
+        ...state,
+        pics: action.payload,
+        loading: false
+      };
+    case ADD_PIC:
+      return {
+        ...state,
+        pics: [action.payload, ...state.pics]
+      };
+    case DELETE_PIC:
+      return {
+        ...state,
+        pics: state.pics.filter(pic => pic._id !== action.payload)
+      };
     default:
       return state;
   }
