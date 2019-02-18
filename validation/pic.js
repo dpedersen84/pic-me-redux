d6f0e007a463e15fe68093f77987c02843efd9e4
Dan <dpedersen84@gmail.com>
2019-02-17 19:18:10 -0600

    validation edit for caption requirement

--- validation/pic.js
+++ validation/pic.js
@@ -5,9 +5,14 @@ module.exports = function validatePicInput(data) {
   let errors = {};
 
   data.image = !isEmpty(data.image) ? data.image : "";
+  data.caption = !isEmpty(data.caption) ? data.caption : "";
 
   if (Validator.isEmpty(data.image)) {
     errors.image = "You must select an image";
+  }
+
+  if (Validator.isEmpty(data.caption)) {
+    errors.caption = "You must enter a caption for your image";
   }
 
   return {
